0b1a323e4d5333481522c071236da47fea8b6bd8
laserpui <nattachai_p@hotmail.com>
2026-09-08 14:28:39 +0700

    Add files via upload

---
+++ pm-platinum-plan/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  // วาง URL ที่ได้จาก Google Apps Script > Deploy > Web app
+  // ต้องลงท้ายด้วย /exec
+  API_URL: "https://script.google.com/macros/s/AKfycbzLoVwpRogRB0rapy0ga5_VzSY8jrJM20768kl7KJns8r25ahYYQY4Lw8_YaYKdbwuQ/exec"
+};
